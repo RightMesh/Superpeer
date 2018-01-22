a470e5c8a1d7cb66ae14b90c6c50b5ecec02349a
Jason Ernst <jason@left.io>
2018-01-22 09:13:16 -0800

    added a function which fires when transactions are sent to the superpeer

--- src/main/java/SuperPeer.java
+++ src/main/java/SuperPeer.java
@@ -4,14 +4,33 @@
 import io.left.rightmesh.id.MeshID;
 import io.left.rightmesh.mesh.JavaMeshManager;
 import io.left.rightmesh.mesh.MeshManager;
+import io.left.rightmesh.mesh.MeshManager.MeshTransactionEvent;
+import io.left.rightmesh.util.MeshUtility;
 
 import java.io.Console;
+
+import static io.left.rightmesh.mesh.MeshManager.TRANSACTION_RECEIVED;
  
-public class SuperPeer implements Transactionlistener {
+public class SuperPeer {
+
+    public static final String TAG = SuperPeer.class.getCanonicalName();
+        
     public static void main(String[] args) {
+        SuperPeer p = new SuperPeer();
+    }
+    
+    public SuperPeer() {
         JavaMeshManager mm = new JavaMeshManager(true);
-        mm.on();
+        mm.on(TRANSACTION_RECEIVED, this::handleTransactionPacket);
         System.out.println("Superpeer MeshID: " + mm.getUuid());
+        
+        System.out.println("Waiting for lib to be ready");
+        try {
+                Thread.sleep(200);
+        } catch(Exception ex) {}
+        
+        //test send on the TRANSACTION PORT
+        mm.sendDataReliable(mm.getUuid(), MeshUtility.TRANSACTION_PORT, "test".getBytes());
         
         //Continue to run until user quits
         System.out.println("Enter empty line to quit");
@@ -30,9 +49,7 @@ public class SuperPeer implements Transactionlistener {
         mm.stop();
     }
     
-    public void handleTransactionPacket() {
-            MeshUtility.Log(TAG, "TRANSACTION READY TO ROCK");
-    }
-    
-    
+    public void handleTransactionPacket(MeshManager.RightMeshEvent e) {
+            System.out.println("TRANSACTION READY TO ROCK!");
+    }  
 }
